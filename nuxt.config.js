794164f1927003430e06d5e147cbd2b5783f23a6
Tom <Tom@Thomass-iMac.local>
2018-11-16 06:42:52 -0500

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'barber-pond',
+    title: 'Barber Pond Road Self Storage',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Barber Pond Rd. Self Storage' }
+      { hid: 'description', name: 'description', content: 'Affordable Self Storage in Pownal VT' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
